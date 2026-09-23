333a12f78b27ee748742084010c133fabe46ee76
Hajira Siraj <hajirasiraj217@gmail.com>
2026-09-23 04:24:31 -0700

    Time: 0 ms (100%), Space: 19.3 MB (43.25%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
         dummy = ListNode()
         cur = dummy
 
